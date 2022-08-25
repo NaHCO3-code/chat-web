cf1709f85dba664696cc94a42fd328000d552a00
shr-NaHCO3 <shrshr2@163.com>
2022-08-25 17:28:34 +0800

    common update
    none

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { config } from './config.js'
 import { cp } from './modules/chatp.js'
 import { safe } from './modules/safe.js'
+import md5 from './modules/md5.js'
 
 export default {
     data(){
@@ -10,11 +11,23 @@ export default {
         }
     },
     mounted(){},
-    methods: {}
+    methods: {
+        register(){
+            cp.sendEncrypted(
+                config.socket.server.publicKey,
+                'register',
+                JSON.stringify({
+                    username: "xiaosuda",
+                    password: md5('114514')
+                })
+            )
+        }
+    }
 }
 </script>
 
 <template>
+    <button @click="register()">register</button>
 </template>
     
 <style scoped>
