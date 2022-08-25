9b0b8c1f36d2a48763ff90ef1f1d7afed9fcfc8d
laizq <123632704@qq.com>
2022-08-25 18:27:30 +0800

    使用setup写法

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
-<script>
+<script setup>
 import { config } from './config.js'
 import { cp } from './modules/chatp.js'
 import { safe } from './modules/safe.js'
 import md5 from './modules/md5.js'
 
-export default {
-    data(){
-        return {
-            
-        }
-    },
-    mounted(){},
-    methods: {
-        register(){
-            cp.sendEncrypted(
-                config.socket.server.publicKey,
-                'register',
-                JSON.stringify({
-                    username: "xiaosuda",
-                    password: md5('114514')
-                })
-            )
-        }
-    }
+function register() {
+    cp.sendEncrypted(
+        config.socket.server.publicKey,
+        'register',
+        JSON.stringify({
+            username: "xiaosuda",
+            password: md5('114514')
+        })
+    )
 }
 </script>
 
